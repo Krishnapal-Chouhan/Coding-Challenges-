3a64db3d0e4f4f02d558f8d33eb7e56d6eeccb05
Krishnapal Chouhan <krishnapalofficial04@gmail.com>
2026-09-22 16:58:01 +0530

    Time: 3 ms (17.82%), Space: 47.7 MB (57.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -13,7 +13,11 @@ class Solution {
                 j++;
 
             }
-           else if(nums[i] ==0 && nums[j]==0){
+            else if (nums[i] != 0 && nums[j] != 0){
+                
+                i++;
+                j++;
+            }else if(nums[i] ==0 && nums[j]==0){
                
                 j++;
             }else{
